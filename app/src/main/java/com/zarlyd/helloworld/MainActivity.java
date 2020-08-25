a7902240f9cec0c9f7fd3bb1af138ae9ff4fe46e
lzafmn <lzafmn@gmail.com>
2020-08-25 11:01:46 +0800

    test git

--- app/src/main/java/com/zarlyd/helloworld/MainActivity.java
+++ app/src/main/java/com/zarlyd/helloworld/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+//test git
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
